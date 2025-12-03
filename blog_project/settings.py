7fb8aecf883ffce3d6f711184636219633fb85b9
DavidAnato <david1anato@gmail.com>
2025-12-03 14:18:21 +0100

    Update ALLOWED_HOSTS to allow all hosts for development

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4%=htvk6_g^%2b-di&_sf8-_-7z-+dcd@zr@9fqhuc)s1v09z_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
