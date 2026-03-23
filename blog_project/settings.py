62fa9c82092f11ed81a7dcdec3ab14ae5d372e09
DavidAnato <david1anato@gmail.com>
2026-03-23 12:05:02 +0100

    feat: add S3 media storage backend support
    Add django-storages and boto3 dependencies to requirements.txt.
    Introduce MEDIA_BACKEND environment variable to toggle between local
    and S3 storage. Configure S3 settings when MEDIA_BACKEND is 's3'.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'blog',  # Notre application
     'dashboard',  # Admin personnalisé ultra stylé
     'blog_project', # Pour charger les traductions JSON au démarrage
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -149,9 +150,22 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Media files (uploads)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_BACKEND = os.environ.get('MEDIA_BACKEND', 'local')
+if MEDIA_BACKEND == 's3':
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN', f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com')
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+else:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
